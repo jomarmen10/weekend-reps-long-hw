160082853ee4029c8012f7fc564e6d0f93aee9b2
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-22 14:09:57 -0700

    Variables & Datatypes done

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log("weekend HW")
+/*
+How do we assign a value to a variable?
+we can assign using let var and const
+
+How do we change the value of a variable?
+we can use =
+
+How do we assign an existing variable to a new variable?
+you can assign it by declaring a new variable and using = to assign the new variable to an existing variable
+
+Remind me, what are declare, assign, and define?
+declaring variable is creating a new variable
+assigning it is giving a value to the variable
+difine is giving the variable multiple value
+
+What is pseudocoding and why should you do it?
+to write code in plain english so that you can construct the process more easily
+
+What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+75% - 90% of thingking and 25%-75% of coding; */
