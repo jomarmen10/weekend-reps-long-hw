fda468726bc072f86ba2681eefe2ea38d31f5aa0
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-22 14:17:45 -0700

    Strings

--- js/app.js
+++ js/app.js
@@ -19,3 +19,25 @@ to write code in plain english so that you can construct the process more easily
 
 What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
 75% - 90% of thingking and 25%-75% of coding; */
+
+
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+let firstVariable = "hello world";
+firstVariable = 12;
+let secondVariable = firstVariable;
+secondVariable = "to string";
+console.log(firstVariable)
+
+
+// Create a variable called yourName and set it equal to your name as a string.
+//  Then, write an expression that takes the string "Hello, my name is " and the variable
+//  yourName so that it returns a new string with them concatenated.
+
+let yourName = "jomar"
+
+let sayName = console.log("hello, my name is", yourName)
