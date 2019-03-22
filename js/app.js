160f01da80b1e116d5e8da360f07781f92556520
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-22 16:05:28 -0700

    what's in your closet

--- js/app.js
+++ js/app.js
@@ -267,3 +267,13 @@ if(number < 5){
 } else {
   console.log("monkey")
 }
+
+///////I've DONE it///////////////
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+// In the same way, access one item from Thom's pants array.
+// Access one item from Thom's accessories array.
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
